2b6d848af1204063adb423ada7ad2e0dba93a743
Asjad <asjadanis128@gmail.com>
2019-01-20 17:53:40 +0500

    Updates

---
+++ precache-manifest.93cc002c65faef011ce9be8800a33677.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2fd244821a5987df3b9f",
+    "url": "/portfolio/static/js/main.2fd24482.chunk.js"
+  },
+  {
+    "revision": "d9832754adc9c1ab602a",
+    "url": "/portfolio/static/js/1.d9832754.chunk.js"
+  },
+  {
+    "revision": "2fd244821a5987df3b9f",
+    "url": "/portfolio/static/css/main.7a079773.chunk.css"
+  },
+  {
+    "revision": "eee6a2c916844e4fb3d775167e82fdb8",
+    "url": "/portfolio/index.html"
+  }
+];
